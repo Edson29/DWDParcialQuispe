f1ac9909d7dafb0294147c6f843837fd582ea673
Edson Quispe <edson.quispef@pucp.pe>
2023-07-28 17:48:38 -0500

    Creacion de modelos Producto/Tiendas

--- examenParcial/gestionTienda/models.py
+++ examenParcial/gestionTienda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Tienda(models.Model):
+    direccion = models.CharField(max_length=50, blank=True, null=True)
+    provincia = models.CharField(max_length=50, blank=True, null=True)
+    region = models.CharField(max_length=50, blank=True, null=True)
+    fechaCreacion = models.DateField( blank=True, null=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+
+
+class Producto(models.Model):
+    descripcion = models.CharField(max_length=50, blank=True, null=True)
+    codigo = models.CharField(max_length=50, blank=True, null=True)
+    precioVenta = models.FloatField(blank=True, null=True)
+    cantidad = models.IntegerField(blank=True, null=True)
+    tiendaRelacionada = models.OneToOneField(Tienda, on_delete=models.SET_NULL, blank=True, null=True)
