1b492145292b744277cfa4e63be4024cd98eb207
Edson Quispe <edson.quispef@pucp.pe>
2023-07-28 19:16:56 -0500

    Creación vista de gestion tiendas

--- examenParcial/gestionTienda/views.py
+++ examenParcial/gestionTienda/views.py
@@ -1,20 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Tienda, Producto
+from datetime import date
 
 # Create your views here.
 
-def index(request):
-    #return HttpResponse("Bienvenidos a la Aplicacion del Examen Parcial")
+def index(request):    
     return render(request, 'index.html')
 
-def productos(request):
-    #return HttpResponse("Vista de PRODUCTOS")
-    return render(request, 'productos.html')
+def productos(request):    
+    return render(request, 'productos.html', {
+        'listaProductos': Producto.objects.all().order_by('id')
+    })
 
 def tiendas(request):
-    #return HttpResponse("Vista de TIENDAS")
-    return render(request, 'tiendas.html')
+    if request.method=='POST':
+        direccionTienda = request.POST.get('direccionTienda')
+        provinciaTienda = request.POST.get('provinciaTienda')        
+        regionTienda = request.POST.get('regionTienda')
+        fechaCreacion = date.today()
+        telefonoTienda = request.POST.get('telefonoTienda')
+        Tienda.objects.create(
+            direccion = direccionTienda,
+            provincia = provinciaTienda,
+            region = regionTienda,
+            fechaCreacion = fechaCreacion,
+            telefono = telefonoTienda,
+        )
+        return HttpResponseRedirect(reverse('gestionTienda:tiendas'))    
+    return render(request, 'tiendas.html', {
+        'listaTiendas': Tienda.objects.all().order_by('id')
+    })
 
-def detalle(request):
-    #return HttpResponse("Vista de detalle de TIENDAS")
+def detalle(request):    
     return render(request, 'detalle.html')
+
+def registroTienda(request):
+    if request.method=='POST':
+        direccionTienda = request.POST.get('direccionTienda')
+        provinciaTienda = request.POST.get('provinciaTienda')        
+        regionTienda = request.POST.get('regionTienda')
+        fechaCreacion = date.today()
+        telefonoTienda = request.POST.get('telefonoTienda')
+        Tienda.objects.create(
+            direccion = direccionTienda,
+            provincia = provinciaTienda,
+            region = regionTienda,
+            fechaCreacion = fechaCreacion,
+            telefono = telefonoTienda,
+        )
+        return HttpResponseRedirect(reverse('gestionTienda:tiendas'))
+    
+        
